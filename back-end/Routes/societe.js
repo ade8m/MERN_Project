ab5b9f2bf4ceed4935ff782d2042c70c18aa793a
ade8m <benammaradem83@gmail.com>
2023-06-15 14:28:32 +0100

    add route

--- back-end/Routes/societe.js
+++ back-end/Routes/societe.js
@@ -1,5 +1,5 @@
 const express =require('express');
-const router =express.Router();
+const router = express.Router();
 const societeControl = require('../controller/societe');
 
 router.post("/New",societeControl.addsociete);
