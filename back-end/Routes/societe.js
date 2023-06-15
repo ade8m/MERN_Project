b16dc31d2abc749d432436af2d43eb4c103cc846
ade8m <benammaradem83@gmail.com>
2023-06-15 13:52:51 +0100

    change route

--- back-end/Routes/societe.js
+++ back-end/Routes/societe.js
@@ -3,8 +3,8 @@ const router =express.Router();
 const societeControl = require('../controller/societe');
 
 router.post("/New",societeControl.addsociete);
-router.post("/update",societeControl.UpdateSociete);
-router.post("/delete",societeControl.DeleteSociete);
-router.post("/get",societeControl.getSociete);
+router.post("/update:id",societeControl.UpdateSociete);
+router.delete("/delete:id",societeControl.DeleteSociete);
+router.get("/get:id",societeControl.getSociete);
 
 module.exports = router;
