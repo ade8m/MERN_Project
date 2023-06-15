0493a8a23b3e58917e4b22d58f1f189e6a9027e9
ade8m <benammaradem83@gmail.com>
2023-06-15 12:13:38 +0100

    create voitur model

---
+++ back-end/Models/voiture.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+const voitureSchema = mongoose.Schema({
+    matricule:{type:String},
+    type:{type:String},
+    Model:{type:String},
+    couleur:{type:String},
+    Nassurrance:{type:String},
+    Nvig:{type:String},
+    Nlaiss:{type:String}
+});
+
+module.exports = mongoose.model('voiture',voitureSchema);
