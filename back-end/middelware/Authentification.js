e0aca9793986bea4c50daa5cf3001b5ea1432f5c
ade8m <benammaradem83@gmail.com>
2023-06-19 09:06:31 +0100

    add middelware function

---
+++ back-end/middelware/Authentification.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.verifyToken = (req,res,next)=>{
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({
+          message: 'Unauthorized access',
+        });
+      }
+      try {
+        const decoded = jwt.verify(token,process.env.secret_key );
+        req.user = decoded;
+        next();
+        }catch (error) {
+            return res.status(401).json({
+              message: 'Invalid token',
+            });
+          }
+
+};
+
+exports.verifyAdmin = (req, res, next) => {
+    if (!req.user.isAdmin) {
+      return res.status(403).json({
+        message: 'Access denied',
+      });
+    }
+  
+    next();
+  };
