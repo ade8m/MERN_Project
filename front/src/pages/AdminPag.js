dbdeb6b8ab11d07b85a1b23838e19320b8359508
ade8m <benammaradem83@gmail.com>
2023-06-22 12:06:53 +0100

    create table mangement usr

--- front/src/pages/AdminPag.js
+++ front/src/pages/AdminPag.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Table, Button } from 'react-bootstrap';
 
-export function AdminPage () {
+export function AdminPage() {
+
+
   const [users, setUsers] = useState([]);
+  const [updatedUser, setUpdatedUser] = useState({ name: '', email: '' });
+  const [editingUserId, setEditingUserId] = useState(null);
 
   useEffect(() => {
     fetchUsers();
@@ -11,8 +15,8 @@ export function AdminPage () {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('/api/users'); // Replace with your API endpoint
-      setUsers(response.data.users);
+      const response = await axios.get('http://localhost:3001/api/get');
+      setUsers(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -20,7 +24,24 @@ export function AdminPage () {
 
   const deleteUser = async (userId) => {
     try {
-      await axios.delete(`/api/users/${userId}`); // Replace with your API endpoint
+      await axios.delete(`http://localhost:3001/api/delete/${userId}`);
+      fetchUsers();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateUser = (userId) => {
+    setEditingUserId(userId);
+    const userToUpdate = users.find((user) => user._id === userId);
+    setUpdatedUser({ name: userToUpdate.name, email: userToUpdate.email });
+  };
+
+  const saveUpdatedUser = async (userId) => {
+    try {
+      await axios.put(`http://localhost:3001/api/update/${userId}`, updatedUser);
+      setEditingUserId(null);
+      setUpdatedUser({ name: '', email: '' });
       fetchUsers();
     } catch (error) {
       console.error(error);
@@ -41,22 +62,47 @@ export function AdminPage () {
         </thead>
         <tbody>
           {users.map((user, index) => (
-            <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>
-                <Button variant="danger" onClick={() => deleteUser(user.id)}>
-                  Delete
-                </Button>
-                {/* Add update functionality here */}
-              </td>
-            </tr>
+            <React.Fragment key={user._id}>
+              <tr>
+                <td>{index + 1}</td>
+                <td>{user.Nom}</td>
+                <td>{user.Email}</td>
+                <td>
+                  <Button variant="danger" onClick={() => deleteUser(user._id)}>
+                    Delete
+                  </Button>
+                  <Button variant="info" onClick={() => updateUser(user._id)}>
+                    Update
+                  </Button>
+                </td>
+              </tr>
+              {user._id === editingUserId && (
+                <tr>
+                  <td colSpan="4">
+                    <input
+                      type="text"
+                      placeholder='Entre nouveau nom' 
+                      value={updatedUser.Nom}
+                      onChange={(e) => setUpdatedUser({ ...updatedUser, name: e.target.value })}
+                    />
+                    <input
+                      type="text"
+                      placeholder='Entre nouveau email'
+                      value={updatedUser.email}
+                      onChange={(e) => setUpdatedUser({ ...updatedUser, email: e.target.value })}
+                    />
+                    <Button variant="primary" onClick={() => saveUpdatedUser(user._id)}>
+                      Save
+                    </Button>
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
           ))}
         </tbody>
       </Table>
     </div>
   );
-};
+}
 
 export default AdminPage;
