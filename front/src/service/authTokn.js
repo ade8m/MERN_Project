7d06affae36c503673274eb5c33cd65e65996be6
ade8m <benammaradem83@gmail.com>
2023-07-06 13:00:19 +0100

    add header  token

--- front/src/service/authTokn.js
+++ front/src/service/authTokn.js
@@ -1,26 +1,27 @@
 
-    export const login =(user)=>{
-      console.log('Request Payload:', user);
-        return fetch('http://localhost:3001/auth/login/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              
-            },
-            body: JSON.stringify(user),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('Login Response:', data);
-              const token = data.token;
-              localStorage.setItem('token', token);
-              return data;
-            })
-            .catch((error) => {
-              console.error('Error:', error);
-              throw error;
-            });
-        };
+export const login = (user) => {
+
+ 
+  
+  return fetch('http://localhost:3001/auth/login/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':  `Bearer ${localStorage.getItem('token')}`
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Login response:', data);
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      throw error;
+    });
+};
+
 
   export  const logout=()=>{
         localStorage.removeItem('token');
