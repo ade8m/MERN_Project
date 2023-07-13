cf1c33069f5e7141a9059094e5efd25d85873c47
ade8m <benammaradem83@gmail.com>
2023-07-13 13:26:15 +0100

    add company

--- location/controller/societe.js
+++ location/controller/societe.js
@@ -5,22 +5,27 @@ const Admin = require('../models/admin');
 exports.createCompany = async (req, res) => {
   try {
     const { name, address, adminUsername, adminPassword } = req.body;
-    const databaseName = `${name.toLowerCase().replace(/\s/g, '_')}`;
-    const companyDb = mongoose.createConnection(process.env.MONGO);
 
-    
+    // Generate a unique database name for the company
+    const databaseName = name.toLowerCase();
+
+    // Create a connection to the company-specific database
+    const companyDb = mongoose.createConnection(`mongodb+srv://adem:adem@atlascluster.8tnxd7f.mongodb.net/${databaseName}`);
+
+    // Define the Company and Admin models for the company-specific database
     const CompanyModel = companyDb.model('Company', Company.schema);
     const AdminModel = companyDb.model('Admin', Admin.schema);
-   
+
     // Create a new company
     const company = new CompanyModel({
       name,
       address,
       databaseName,
     });
-   
+
     // Save the company to the main database
-    await company.save(); 
+    await company.save();
+
     // Create a new admin
     const admin = new AdminModel({
       username: adminUsername,
@@ -30,7 +35,7 @@ exports.createCompany = async (req, res) => {
 
     // Save the admin to the company-specific database
     await admin.save();
-    
+
     // Associate the admin with the company
     company.admins.push(admin._id);
     await company.save();
