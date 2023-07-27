681d8ee6ce1aa9facea5ff26b7ba924c5d5a7b08
ade8m <benammaradem83@gmail.com>
2023-07-27 10:56:13 +0100

    delete file

---
+++ location/models/location.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+  name: { type: String, unique: true },
+  address: { type: String },
+  admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }],
+  databaseName: { type: String, unique: true },
+});
+
+const Company = mongoose.model('Company', companySchema);
+
+module.exports = Company;
