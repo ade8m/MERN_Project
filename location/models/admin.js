448b164a9a6fe0e9a73fbfa9d25d3abe2acb0f10
ade8m <benammaradem83@gmail.com>
2023-07-31 10:40:42 +0100

    change admin model

--- location/models/admin.js
+++ location/models/admin.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const adminSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   password: { type: String },
-  company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+  companyName: { type: String }, 
+ 
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
