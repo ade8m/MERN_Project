c1b3efaae1101e13173b874d210f0fea4fb8cb00
ade8m <benammaradem83@gmail.com>
2023-08-18 11:05:46 +0100

    add new endpoint

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,8 @@ const voitureRoute = require('./Routes/voiture');
 const AuthRoute = require('./Routes/auth');
 const userRoute = require('./Routes/user');
 const modelRoute = require('./Routes/model');
+const typeRoute = require('./Routes/type');
+const colorRoute = require('./Routes/couleur');
 const app = express();
 require('dotenv').config();
 
@@ -36,6 +38,8 @@ app.use("/auth",AuthRoute);
 app.use("/api",userRoute);
 app.use("/societe",societeRoute);
 app.use("/model",modelRoute);
+app.use("/type",typeRoute);
+app.use("/color",colorRoute);
 
 
 //Create Server
