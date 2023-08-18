3272f2d896233b636a6f2d15a3e494dfa0edfdc8
ade8m <benammaradem83@gmail.com>
2023-08-18 10:11:09 +0100

    add unique condution

--- back-end/Models/couleur.js
+++ back-end/Models/couleur.js
@@ -1,8 +1,8 @@
 const mongoose =require('mongoose');
 const colorSchema = mongoose.Schema({
 
-    color:{type:String
-        }
+    color:{type:String,
+       unique:true }
 
 });
 module.exports = mongoose.model('couleur',colorSchema);
